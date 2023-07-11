8c205d89c6a906df28fa6f986e746895b43634d6
andriiko <demigod03_blusher@icloud.com>
2023-07-10 22:48:25 -0400

    Add styles

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,81 @@
+<!--https://youtu.be/XzLuMtDelGk?t=1801-->
+
 <script>
 export default {
   data() {
     return {
-      likes: 0,
-      dislikes: 0,
+      posts: [
+        {id: 0, title: "JavaScript post", body: "Post description"},
+        {id: 1, title: "JavaScript post", body: "Post description"},
+        {id: 2, title: "JavaScript post", body: "Post description"},
+        {id: 3, title: "JavaScript post", body: "Post description"},
+      ]
+
     }
   },
   methods:{
-    addlike() {
-      this.likes += 1;
+      createPost() {
+        
+      }
     },
-    adddislike() {
-      this.dislikes += 1;
-    },
-  },
+
 }
 </script>
 
 <template>
-  <div>
-    <div>
-      <button @click="addlike">Like</button>
-      <button @click="adddislike">Dislike</button>
+  <div class="app">
+    <form>
+      <input class="input" type="text" placeholder="Title">
+      <input class="input" type="text" placeholder="Description">
+      <button class="btn" @click="createPost">Create</button>
+
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Title:</strong> {{post.title}}#{{post.id}}</div>
+      <div><strong>Description:</strong>{{post.body}} {{post.id + 1}}</div>
     </div>
-    <div>Likes {{ likes }}</div>
-    <div>DisLikes {{ dislikes }} </div>
   </div>
 
 </template>
 
-<style scoped>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  padding: 20px;
+}
+
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-bottom: 15px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  margin-bottom: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+  
+}
 
 </style>
