30c9d647f65db5cf660d6802ee32d7b4f1b901f8
andriiko <demigod03_blusher@icloud.com>
2023-07-10 21:18:26 -0400

    Base functions of Vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    }
+  },
+  methods:{
+    addlike() {
+      this.likes += 1;
+    },
+    adddislike() {
+      this.dislikes += 1;
+    },
+  },
+}
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <div>
+      <button @click="addlike">Like</button>
+      <button @click="adddislike">Dislike</button>
+    </div>
+    <div>Likes {{ likes }}</div>
+    <div>DisLikes {{ dislikes }} </div>
+  </div>
+
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style scoped>
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
