31e19ead945090ae65592d08ffb91294d6f49828
andriiko <demigod03_blusher@icloud.com>
2023-07-11 12:59:49 -0400

    Add form to append new posts to posts array

--- src/App.vue
+++ src/App.vue
@@ -9,30 +9,58 @@ export default {
         {id: 1, title: "JavaScript post", body: "Post description"},
         {id: 2, title: "JavaScript post", body: "Post description"},
         {id: 3, title: "JavaScript post", body: "Post description"},
-      ]
-
+      ],
+      title: '',
+      body: '',
     }
   },
-  methods:{
-      createPost() {
-        
+  methods: {
+    createPost() {
+      if (this.body.length === 0) {
+        return;
       }
+      if (this.title.length === 0) {
+        return;
+      }
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+      this.posts.push(newPost);
+      this.title = '';
+      this.body = '';
     },
 
+  },
 }
 </script>
 
 <template>
   <div class="app">
-    <form>
-      <input class="input" type="text" placeholder="Title">
-      <input class="input" type="text" placeholder="Description">
-      <button class="btn" @click="createPost">Create</button>
+    <form @submit.prevent>
+      <h4>Post creation</h4>
+
+      <input class="input"
+             placeholder="Title"
+             type="text"
+             v-bind:value="title"
+             @input="title=$event.target.value"/>
+
+      <input class="input"
+             placeholder="Description"
+             type="text"
+             v-bind:value="body"
+             @input="body=$event.target.value"/>
+
+      <button class="btn"
+              @click="createPost">Create
+      </button>
 
     </form>
     <div class="post" v-for="post in posts">
-      <div><strong>Title:</strong> {{post.title}}#{{post.id}}</div>
-      <div><strong>Description:</strong>{{post.body}} {{post.id + 1}}</div>
+      <div><strong>Title:</strong> {{ post.title }}#{{ post.id }}</div>
+      <div><strong>Description:</strong>{{ post.body }} {{ post.id + 1 }}</div>
     </div>
   </div>
 
@@ -75,7 +103,7 @@ form {
   background: none;
   color: teal;
   border: 1px solid teal;
-  
+
 }
 
 </style>
